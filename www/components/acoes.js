73784a8c02b65060aa88e61629b5e9dc9d95eb70
CristineNay <cristine.nay15@gmail.com>
2019-02-28 03:32:05 +0900

    Inserting function and values in buttons

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -1,2 +1,9 @@
-// This is a JavaScript file
+$(document).on('click','.btns',function(){
+  var valor = $('#display').val();
+  valor += this.value;
+  $('#display').val(valor);
+});
 
+$(document).on('click','#ce',function(){
+  $('#display').val("");
+});
